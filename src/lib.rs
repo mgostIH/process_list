a631744bdadb5577aa3b5ff55a54398ac1d4ad5e
mgostIH <dennizmodesti@gmail.com>
2020-01-12 18:41:56 +0100

    Fixed documentation a bit for crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
-//! Process Listing.
-//!
+//! # Process Listing
 //! This crate exposes a `for_each_processes` function to deal sequentially with every process open in the operating system.
+//! 
+//! ## Example
+//! Printing every process to `stdout`
+//! ```
+//! use process_list::for_each_process;
+//! fn print_processes(id : u32, name : &str) {
+//!     println!("Id: {} --- Name: {}", id, name);
+//! }
+//! for_each_process(print_processes).unwrap();
+//! ```
+//! 
+//! # Support
+//! For now only Windows is supported, but it should be simple enough to port on other operating systems. 
+//! 
+//! It's not a priority but pull requests are well accepted.
+
 
 // We need macros **BEFORE** defining the modules that use them
 #[allow(unused)]
